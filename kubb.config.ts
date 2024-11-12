f7ec43b7b12dffbdbd45bfd52e7aa06dffac4fa0
Aaron Burtnyk <aaron.burtnyk@candide.com>
2024-11-12 17:49:03 +0000

    Update to v3

---
+++ kubb.config.ts
@@ -0,0 +1,26 @@
+import {defineConfig}  from '@kubb/core'
+import {pluginOas}  from '@kubb/plugin-oas'
+import {pluginTs}  from '@kubb/plugin-ts'
+
+export default defineConfig({
+  root: '.',
+  input: {
+    path: 'docs/main.yml'
+  },
+  output: {
+    path: 'generated/',
+    barrelType: 'all',
+    clean: true
+  },
+  plugins: [
+    pluginOas({
+      generators: []
+    }),
+    pluginTs({
+      output: {
+        path: "./types.ts",
+        barrelType: false
+      }
+    })
+  ]
+})
